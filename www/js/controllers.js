fe0982056181a7806b4b427bbdaad135ebd8e507
Milica Jovanovic <j.milica95@yahoo.com>
2017-05-29 22:59:23 +0200

    Kreirano i prikazano ocenjivanje postojecih testova, kao i odredjivanje ukupne ocene

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,5 @@
 angular.module('starter.controllers', [])
 
-
 .controller('HomeCtrl', function($scope, $state) {
   
   $scope.startTesting = function() {
@@ -20,30 +19,44 @@ angular.module('starter.controllers', [])
 
   $scope.startTesting = function() {
     showProgress();
+    $scope.markFactorial = 0;
+    $scope.markCount = 0;
+    $scope.markGcd = 0;
+    $scope.finalMark = 0;
     setTimeout( 
       function() {
         var startFactorial = new Date().getTime();
-        console.log(factorial(5));
+        console.log(factorial(1000));
         var endFactorial = new Date().getTime();
         var timeFactorial = endFactorial - startFactorial;
         console.log('Execution time: ' + timeFactorial);
+        $scope.markFactorial = rateFactorial(timeFactorial);
+        console.log($scope.markFactorial);
 
         var startCount = new Date().getTime();
-        console.log(count(5));
+        console.log(count(1000));
         var endCount = new Date().getTime();
         var timeCount = endCount - startCount;
         console.log('Execution time: ' + timeCount);
+        $scope.markCount = rateCount(timeCount);
+        console.log($scope.markCount);
 
         var startGcd = new Date().getTime();
         console.log(gcd(5005, 2222));
         var endGcd = new Date().getTime();
         var timeGcd = endGcd - startGcd;
         console.log('Execution time: ' + timeGcd);
+        $scope.markGcd = rateGcd(timeGcd);
+        console.log($scope.markGcd);
 
+        $scope.finalMark = ($scope.markFactorial + $scope.markCount + $scope.markGcd) / 3;
+        console.log($scope.finalMark);
+        document.getElementById('result').style.display = 'block';
       }, 3000);
   }
 
   function showProgress() {
+    document.getElementById('result').style.display = 'none';
     document.getElementById('startTestingBtn').style.display = 'none';
     document.getElementById('firstText').style.display = 'none';
     document.getElementById('thirdText').style.display = 'none';
@@ -55,6 +68,58 @@ angular.module('starter.controllers', [])
     document.getElementById('loading').style.display = 'none';
     document.getElementById('secondText').style.display = 'none';
     document.getElementById('thirdText').style.display = 'block';
+    document.getElementById('result').style.display = 'block';
+  }
+
+  function rateFactorial(time) {
+    var mark = 0;
+    if (time <= 5) {
+        mark = 5;
+    } else if (time > 5 && time < 7) {
+        mark = 4;
+    } else if (time > 7 && time < 10) {
+        mark = 3;
+    } else if (time > 10 && time < 15) {
+        mark = 2;
+    } else if (time > 15) {
+        mark = 1;
+    }
+
+    return mark;
+  }
+
+  function rateCount(time) {
+    var mark = 0;
+    if (time <= 5) {
+        mark = 5;
+    } else if (time > 5 && time < 7) {
+        mark = 4;
+    } else if (time > 7 && time < 10) {
+        mark = 3;
+    } else if (time > 10 && time < 15) {
+        mark = 2;
+    } else if (time > 15) {
+        mark = 1;
+    }
+
+    return mark;
+  }
+
+  function rateGcd(time) {
+    var mark = 0;
+    if (time <= 2) {
+        mark = 5;
+    } else if (time > 2 && time < 4) {
+        mark = 4;
+    } else if (time > 4 && time < 6) {
+        mark = 3;
+    } else if (time > 6 && time < 8) {
+        mark = 2;
+    } else if (time > 8) {
+        mark = 1;
+    }
+
+    return mark;
   }
 
   function factorial(n) {
